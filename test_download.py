a6ad64f8e48aa0401f5a876d48d04cc92ec21d65
Jiapeng Zhang <46623500+zjp-shadow@users.noreply.github.com>
2024-07-09 17:15:25 +0800

    Delete test_download.py

--- test_download.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import hf_hub_download, list_repo_files
-
-repo_id = "zjpshadow/CharacterGen"
-all_files = list_repo_files(repo_id, revision="main")
-
-for file in all_files:
-    if file.startswith("2D_Stage") or file.startswith("3D_Stage"):
-        hf_hub_download(repo_id, file, local_dir=".")
